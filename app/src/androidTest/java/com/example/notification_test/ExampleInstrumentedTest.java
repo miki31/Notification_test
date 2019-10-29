7f150b662be85d72c0b0aded1ff12d48156b3ac8
miki31 <it.misha2016@gmail.com>
2019-10-29 16:42:56 +0200

    screen orientation only portrait

--- app/src/androidTest/java/com/example/notification_test/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/notification_test/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.notification_test;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
