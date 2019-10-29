90fe5f727a413c338158d4395b05638eea782bc8
miki31 <it.misha2016@gmail.com>
2019-10-30 00:48:49 +0200

    add dependency ButterKnife

--- app/src/main/java/com/example/notification_test/NotifActivity.java
+++ app/src/main/java/com/example/notification_test/NotifActivity.java
@@ -1,14 +1,24 @@
 package com.example.notification_test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NotifActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.textView)
+    TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notif);
+        ButterKnife.bind(this);
+        mTextView.setText("Hello NOTIFICATION app!!!");
     }
 }
